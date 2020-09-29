86cc26132066f4912d3d532445d197eb2afc0a09
Debian <bitnami@ip-172-26-13-218.us-east-2.compute.internal>
2020-09-29 14:08:32 +0000

    i think it works?

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var app = express();
 var routes = require('./app_server/routes/index');
 
 require('./app_server/models/db',{useUnifiedTopology: true});
+var app = express();
 
+//view engine setup
 app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'ejs');
 
@@ -20,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+   //callback
+app.use(function(req, res, next) { 
   next(createError(404));
 });
 
--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -3,9 +3,30 @@ module.exports.blogadd = function(req, res){
 };
 
 module.exports.bloglist = function(req, res){
-	res.render('blog-list', {title: 'Blog List'});
-};
+	res.render('blog-list', {
+		title: 'Blog List',
+	blogEntries: [{
+		blog_title: 'Post 1',
+		blog_text: 'I dont wanna get this bread anymore',
+		createdOn: '9/28/2020'
+	},
 
+	{	
+		blog_title: 'Post 2',
+		blog_text: 'Im tired',
+		createdOn: '9/28/2020'
+	},
+
+	{
+		blog_title: 'Post 3',
+		blog_text: 'I am ready to pass away',
+		createdOn: '9/28/2020'
+	}
+
+	]
+	
+	});
+};
 
 module.exports.blogedit = function(req, res){
 	res.render('blog-edit', {title: 'Blog Edit'});
